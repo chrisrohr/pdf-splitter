e8cfb4f8f24bb67615ddd69cc4ae3ec1b052c962
Chris Rohr <51920+chrisrohr@users.noreply.github.com>
2020-11-10 11:42:38 -0500

    Upgrade pdfbox

--- src/main/java/com/evanguardsolutions/pdfsplit/PDFSplitter.java
+++ src/main/java/com/evanguardsolutions/pdfsplit/PDFSplitter.java
@@ -1,116 +1,95 @@
 package com.evanguardsolutions.pdfsplit;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.List;
-
-import org.apache.pdfbox.exceptions.COSVisitorException;
+import org.apache.pdfbox.io.RandomAccessBufferedFileInputStream;
+import org.apache.pdfbox.multipdf.Splitter;
 import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdfwriter.COSWriter;
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.util.Splitter;
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 
 public class PDFSplitter {
 	public static void main(String[] args) {
-		try {
-			// Checks the program arguments to make sure the original file has been provided
-			if (args.length == 0) {
-				System.out.println("Err: NONE Missing original file");
-				System.exit(1);
+		// Checks the program arguments to make sure the original file has been provided
+		if (args.length == 0) {
+			System.out.println("Err: NONE Missing original file");
+			System.exit(1);
+		}
+
+		String originalFile = args[0];
+
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			 PDDocument document = parseDocument(originalFile)) {
+
+			int totalPages = document.getNumberOfPages();
+
+			System.out.println("Ready:");
+
+			// Reading each line of the standard input.  Will exit when quit is encountered (or the input stream is closed).
+			String inLine;
+			while (!(inLine = br.readLine()).equals("quit")) {
+				Splitter splitter = new Splitter();
+
+				// Splitting out the pieces of the read line. Format: start_page end_page filename
+				String[] inputSplit = inLine.split(" ");
+
+				int startPage = Integer.parseInt(inputSplit[0]);
+				int endPage = Integer.parseInt(inputSplit[1]);
+				String outFile = inputSplit[2];
+
+				if (startPage > totalPages) {
+					// Outside page range, sending error and continue
+					System.out.println("Err: " + outFile + " Start page [" + startPage + "] or End page [" + endPage + "] out of range.  Total [" + totalPages + "]");
+					continue;
+				}
+
+				if (endPage > totalPages) {
+					endPage = totalPages;
+				}
+
+				// Because this library expects to split a file evenly, we are tricking it by setting the start page, end page, and setting the split = the end page (making one output)
+				splitter.setStartPage(startPage);
+				splitter.setEndPage(endPage);
+				splitter.setSplitAtPage(endPage);
+
+				// Split the document and write out the new file.
+				splitAndWriteNewFile(document, splitter, outFile);
 			}
-			
-			String originalFile = args[0];
-			
-			InputStream input = null;
-			PDDocument document = null;
-			BufferedReader br = null;
-			int totalPages = 0;
-			
-			try {
-				// This try block tries to read the original file and parse the PDF document
-				try {
-					input = new FileInputStream(originalFile);
-					document = parseDocument(input);
-					totalPages = document.getNumberOfPages();
-				} catch (Exception e) {
-					System.out.println("Err: NONE Unable to read file [" + originalFile + "]");
-					System.exit(1);
-				}
-				
-				System.out.println("Ready:");
-				
-				// Setting up to read from standard in
-				br = new BufferedReader(new InputStreamReader(System.in));
-				
-				// Reading each line of the standard input.  Will exit when quit is encountered (or the input stream is closed).
-				String inLine;
-				while (!(inLine=br.readLine()).equals("quit")) {
-					Splitter splitter = new Splitter();
-					
-					// Splitting out the pieces of the read line. Format: start_page end_page filename
-					String[] inputSplit = inLine.split(" ");
-					
-					int startPage = Integer.parseInt(inputSplit[0]);
-					int endPage = Integer.parseInt(inputSplit[1]);
-					String outFile = inputSplit[2];
-					
-					if (startPage > totalPages) {
-						// Outside page range, sending error and continue
-						System.out.println("Err: " + outFile + " Start page [" + startPage + "] or End page [" + endPage + "] out of range.  Total [" + totalPages + "]");
-						continue;
-					}
-					
-					if (endPage > totalPages) {
-						endPage = totalPages;
-					}
-					
-					// Because this library expects to split a file evenly, we are tricking it by setting the start page, end page, and setting the split = the end page (making one output)
-					splitter.setStartPage(startPage);
-					splitter.setEndPage(endPage);
-					splitter.setSplitAtPage(endPage);
-					
-					// Split the document and write out the new file.
-					try {
-						List<PDDocument> documents = splitter.split(document);
-						
-						if (documents.size() > 0) {
-							PDDocument doc = documents.get(0);
-							writeDocument(doc, outFile);
-							doc.close();
-							System.out.println("Ok: " + outFile);
-						}
-					} catch (Exception e) {
-						System.out.println("Err: " + outFile + " " + e.getMessage());
-					}
-				}
-			} finally {
-				if (input != null) {
-					input.close();
-				}
-				if (document != null) {
-					document.close();
-				}
-				if (br != null) {
-					br.close();
-				}
-			}
-		} catch (Exception e) {}
-		
+		} catch (Exception e) {
+			System.out.println("Err: NONE Unable to read file [" + originalFile + "]");
+			System.exit(1);
+		}
+
 		System.out.println("Exiting");
+	}
+
+	private static void splitAndWriteNewFile(PDDocument document, Splitter splitter, String outFile) {
+		try {
+			List<PDDocument> documents = splitter.split(document);
+
+			if (!documents.isEmpty()) {
+				PDDocument doc = documents.get(0);
+				writeDocument(doc, outFile);
+				doc.close();
+				System.out.println("Ok: " + outFile);
+			}
+		} catch (Exception e) {
+			System.out.println("Err: " + outFile + " " + e.getMessage());
+		}
 	}
 	
 	/**
 	 * Takes the original file input stream and parses the PDF for manipulating.
-	 * @param input The input stream of the original file
+	 * @param input The file name of the original file
 	 * @return The parsed PDF
 	 * @throws IOException Thrown if there is a problem reading the original PDF
 	 */
-	private static PDDocument parseDocument(InputStream input) throws IOException {
-		PDFParser parser = new PDFParser(input);
+	private static PDDocument parseDocument(String input) throws IOException {
+		PDFParser parser = new PDFParser(new RandomAccessBufferedFileInputStream(input));
 		parser.parse();
 		return parser.getPDDocument();
 	}
@@ -120,24 +99,11 @@ public class PDFSplitter {
 	 * @param doc The PDF to write
 	 * @param filename The filename of the PDF that will be written
 	 * @throws IOException Thrown if there is a problem writing the PDF
-	 * @throws COSVisitorException Thrown if there is a problem with PDF object
 	 */
-	private static final void writeDocument(PDDocument doc, String filename) throws IOException, COSVisitorException {
-		FileOutputStream output = null;
-		COSWriter writer = null;
-		
-		try {
-			output = new FileOutputStream(filename);
-			writer = new COSWriter(output);
+	private static void writeDocument(PDDocument doc, String filename) throws IOException {
+
+		try (FileOutputStream output = new FileOutputStream(filename); COSWriter writer = new COSWriter(output)) {
 			writer.write(doc);
-		} finally {
-			if (output != null) {
-				output.close();
-			}
-			
-			if (writer != null) {
-				writer.close();
-			}
 		}
 	}
 }
